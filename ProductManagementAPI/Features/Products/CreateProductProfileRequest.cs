f7bc356b73fca6e8dd9731169571bf70890aa175
Alexandru Nechifor <alexandrunec29@gmail.com>
2025-10-24 10:54:09 +0300

    Task 1.1 Step 1

---
+++ ProductManagementAPI/Features/Products/CreateProductProfileRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace ProductManagementAPI.Features.Products;
+
+public class CreateProductProfileRequest
+{
+    private string Name;
+    private string Brand;
+    private string SKU;
+    private ProductCategory Category;
+    private decimal Price;
+    private DateTime ReleaseDate;
+    private string? ImageURL;
+    private int StockQuantity = 1;
+}
---
+++ ProductManagementAPI/Features/Products/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace ProductManagementAPI.Features.Products;
+
+public class Product
+{
+    private string Name;
+    private string Brand;
+    private string SKU;
+    private ProductCategory Category;
+    private decimal Price;
+    private DateTime ReleaseDate;
+    private string? ImageURL;
+    private bool IsAvailable;
+    private int StockQuantity = 0;
+}
